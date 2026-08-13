2edc4ffbabe19d9ece04945b10251f6aee016433
Mevlüt Can Tuna <62645675+mevlutcantuna@users.noreply.github.com>
2026-08-13 12:47:59 +0300

    Time: 19 ms (50.39%) | Memory: 65.2 MB (37.12%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.ts
@@ -0,0 +1,22 @@
+function subarraySum(nums: number[], k: number): number {
+    let result = 0
+    let currentSum = 0;
+
+    const prefixSumCount = new Map<number, number>()
+    prefixSumCount.set(0, 1);
+
+    for (const num of nums) {
+        currentSum += num;
+
+        const needed = currentSum - k;
+
+        result += prefixSumCount.get(needed) ?? 0;
+
+        prefixSumCount.set(
+            currentSum,
+            (prefixSumCount.get(currentSum) ?? 0) + 1
+        );
+    }
+
+    return result;
+};
