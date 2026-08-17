8c044d1ec9c62cfca11b2281aaf05254dffd77bb
Mevlüt Can Tuna <62645675+mevlutcantuna@users.noreply.github.com>
2026-08-17 13:24:00 +0300

    Time: 2 ms (53.56%) | Memory: 55.2 MB (84.13%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.ts
@@ -0,0 +1,14 @@
+function longestCommonPrefix(strs: string[]): string {
+    for(let i = 0; i < strs[0].length; i++){
+        const char = strs[0][i]; // give us chars of first word
+
+        for(let j = 1; j < strs.length; j++){
+            if(i >= strs[j].length || strs[j][i] !== char){
+                 return strs[0].slice(0, i);
+            }
+        }
+    }
+
+
+    return strs[0];
+};
