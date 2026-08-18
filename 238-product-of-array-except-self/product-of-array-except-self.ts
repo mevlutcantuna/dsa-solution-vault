7b1d66b0fc30fc22d404fa825c7379f371bd71bc
Mevlüt Can Tuna <62645675+mevlutcantuna@users.noreply.github.com>
2026-08-18 15:25:28 +0300

    Time: 5 ms (79.81%) | Memory: 69.5 MB (80.55%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.ts
+++ 238-product-of-array-except-self/product-of-array-except-self.ts
@@ -1,17 +1,16 @@
 function productExceptSelf(nums: number[]): number[] {
-    let result: number[] = new Array(nums.length).fill(1);
-
+    let leftProducts = [];
     let leftProduct = 1;
-    for(let i = 0; i < nums.length; i++){
-        result[i] = leftProduct;
-        leftProduct *= nums[i];
+    for (let i = 0; i < nums.length; i++) {
+        leftProducts[i] = leftProduct;
+        leftProduct *= nums[i]
     }
 
     let rightProduct = 1;
-    for(let i = nums.length - 1; i >= 0; i--){
-        result[i] *= rightProduct;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        leftProducts[i] = leftProducts[i] * rightProduct
         rightProduct *= nums[i]
     }
 
-    return result
+    return leftProducts
 };
