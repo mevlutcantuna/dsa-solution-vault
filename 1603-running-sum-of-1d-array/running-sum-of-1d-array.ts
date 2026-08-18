06a3a83f599826b254f32868cacf27f0963db06b
Mevlüt Can Tuna <62645675+mevlutcantuna@users.noreply.github.com>
2026-08-18 14:04:28 +0300

    Time: 0 ms (100.00%) | Memory: 56.7 MB (39.06%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.ts
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.ts
@@ -1,11 +1,10 @@
 function runningSum(nums: number[]): number[] {
-    let result = new Array(nums.length).fill(0);
+    let prefix = 0;
 
-    let preSum = 0;
     for(let i = 0; i < nums.length; i++){
-        result[i] = nums[i] + preSum 
-        preSum += nums[i]
-    } 
+        prefix += nums[i]
+        nums[i] = prefix
+    }
 
-    return result
+    return nums;
 };
